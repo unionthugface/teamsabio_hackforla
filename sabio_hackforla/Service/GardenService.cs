8bc2762bc2004e09e44f34e4ceac58018bfb68ea
Jonathan Lee <lee.yuchan@gmail.com>
2015-06-06 21:23:27 -0700

    add garden service again

---
+++ sabio_hackforla/Service/GardenService.cs
@@ -0,0 +1,21 @@
+﻿using sabio_hackforla.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace sabio_hackforla.Service
+{
+    public class GardenService
+    {
+        public static Garden GetGarden()
+        {
+            Garden myGarden = new Garden();
+            myGarden.ListOfPlants = null;
+            myGarden.Neighborhood = null;
+            myGarden.Zipcode = 0;
+            return myGarden;
+        }
+    }
+}
