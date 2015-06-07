acd53819fa3e6bc2a1abe7cc2c681a80f4174d4e
Jonathan Lee <lee.yuchan@gmail.com>
2015-06-06 22:06:32 -0700

    Adding genereic UserApiController and User Service

---
+++ sabio_hackforla/Controllers/UserApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using sabio_hackforla.Models;
+using System.Web;
+using System.Diagnostics;
+using System.IO;
+
+namespace sabio_hackforla.Controllers
+{
+    [RoutePrefix("api/UserLocation")]
+    public class UserApiController : ApiController
+    {
+    [Route(""),HttpGet]
+        public HttpResponseMessage GetLocation(User model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+            HttpResponseMessage resp = null;
+
+            return resp;
+        }
+    }
+}
