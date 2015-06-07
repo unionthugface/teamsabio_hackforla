3093bf6b2c9aead2cfcd3aa72b6b96111706a4d5
uber manny <elroldan@gmail.com>
2015-06-07 06:16:33 -0700

    adding namespacing for js

---
+++ sabio_hackforla/Scripts/plant.app.js
@@ -0,0 +1,21 @@
+﻿var plant = {
+    layout: {}
+    , page: {
+        handlers: {}
+        , startUp: null
+    }
+    , services: {}
+};
+
+
+plant.layout.startUp = function () {
+
+    console.debug("plant.layout.startUp");
+
+    //this does a null check on plantapp.page.startUp
+    if (plant.page.startUp) {
+        console.debug("plant.page.startUp");
+        plant.page.startUp();
+    }
+};
+$(document).ready(plant.layout.startUp);
