1a7bb60b189ddaede4a406d71b934b62afd22af5
uber manny <elroldan@gmail.com>
2015-06-08 07:32:31 -0700

    added right controller

---
+++ sabio_hackforla/Controllers/IdentificationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sabio_hackforla.Controllers
+{
+    public class IdentificationController : Controller
+    {
+        // GET: Matching
+        public ActionResult Index(string url = "http://c2.staticflickr.com/4/3467/3895548557_5ff7e67db2_n.jpg")
+        {
+            ViewBag.imgurl = url;
+            return View();
+        }
+    }
+}
